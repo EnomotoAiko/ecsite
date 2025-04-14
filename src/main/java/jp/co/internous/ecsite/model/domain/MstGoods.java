6d909efd4b15f9bfc2c45811166c02dcc0ebc0f1
EnomotoAiko <aNuu09_Nuts@outlook.jp>
2025-04-14 20:00:13 +0900

    ecsite

---
+++ src/main/java/jp/co/internous/ecsite/model/domain/MstGoods.java
@@ -0,0 +1,49 @@
+package jp.co.internous.ecsite.model.domain;
+
+import java.sql.Timestamp;
+
+public class MstGoods {
+	
+	private int id;
+	
+	private String goodsName;
+	
+	private int price;
+	
+	private Timestamp createAt;
+	
+	private Timestamp updateAt;
+
+	private Timestamp updatedAt;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+    public String getGoodsName() {
+    	return goodsName;
+    }
+    public void setGoodsName(String goodsName) {
+    	this.goodsName = goodsName;
+    }
+    public int getPrice() {
+    	return price;
+    }
+    public void setPrice(int price) {
+    	this.price = price;
+    }
+    public Timestamp getCreateAt() {
+    	return createAt;
+    }
+    public void setCreatedAt(Timestamp createdAt) {
+    	this.createAt = createdAt;
+    }
+    public Timestamp getUpdatatedAt() {
+    	return updatedAt;
+    }
+    public void setUpdatedAt(Timestamp updatedAt){
+        this.updatedAt = updateAt;
+}
+}
